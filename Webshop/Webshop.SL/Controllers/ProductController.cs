fa95102976c246141e26bf7017b54686ddc5f64a
IsmailAlmaci <38719243+IsmailAlmaci@users.noreply.github.com>
2019-11-09 18:19:20 +0100

    using Dataholder for api data

--- Webshop/Webshop.SL/Controllers/ProductController.cs
+++ Webshop/Webshop.SL/Controllers/ProductController.cs
@@ -5,30 +5,22 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Webshop.BL;
+using Webshop.DAL;
 using Webshop.DAL.Entit;
 
 namespace Webshop.SL.Controllers
 {
     public class ProductController : ApiController
     {
-        private List<Product> SeedList()
-        {
-            List<Product> products = new List<Product>();
-            products.Add(new Product("Video", 120, DateTime.Now, DateTime.Now, null, null, null));
-            products.Add(new Product("Book", 40, DateTime.Now, DateTime.Now, null, null, null));
-            products.Add(new Product("CD", 80, DateTime.Now, DateTime.Now, null, null, null));
-            return products;
-        }
-
         public IEnumerable<Product> Get()
         {
             
-            return SeedList();
+            return DataHolder.GetProducts();
         }
 
         public Product Get(int id)
         {
-            return SeedList().ElementAt(id - 1);
+            return DataHolder.GetProducts().ElementAt(id - 1);
         }
     }
 }
--- Webshop/Webshop.SL/Controllers/ProductPriceController.cs
+++ Webshop/Webshop.SL/Controllers/ProductPriceController.cs
@@ -4,10 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Webshop.DAL;
+using Webshop.DAL.Entit;
 
 namespace Webshop.SL.Controllers
 {
     public class ProductPriceController : ApiController
     {
+        public IEnumerable<ProductPrice> Get()
+        {
+
+            return DataHolder.GetProductPrices();
+        }
+
+        public ProductPrice Get(int id)
+        {
+            return DataHolder.GetProductPrices().ElementAt(id - 1);
+        }
     }
 }
