4ed46cb3adda9b34d85ce9579bfe9d4dd1df7327
Ismail <s104428@ap.be>
2019-11-21 13:59:05 +0100

    hotfix

--- Webshop/Webshop.UI-MVC/APIConsumer.cs
+++ Webshop/Webshop.UI-MVC/APIConsumer.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Web;
-using System.Collections.Generic;
 using System.Web.Script.Serialization;
 
 namespace Webshop.UI_MVC
