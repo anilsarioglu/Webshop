f8b562c1cae34b8851e0bbf94227f79214695cb2
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-11-22 15:10:00 +0100

    Api for InvoiceDetailController.

--- Webshop/Webshop.SL/Controllers/InvoiceDetailController.cs
+++ Webshop/Webshop.SL/Controllers/InvoiceDetailController.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Webshop.BL;
+using Webshop.Domain;
 
 namespace Webshop.SL.Controllers
 {
     public class InvoiceDetailController : ApiController
     {
+        private InvoiceDetailLogic invoiceDetailLogic = new InvoiceDetailLogic();
+
+        public IEnumerable<InvoiceDetailDTO> Get()
+        {
+            return invoiceDetailLogic.GetAll().AsEnumerable();
+        }
+
+        public string Get(int id)
+        {
+            return "test " + id;
+        }
     }
 }
