cd1a3d7252bba509c976985eef1a9c554643aef8
Hamza El Bahri <s109948@ap.be>
2019-11-27 14:25:29 +0100

    VatController

--- Webshop/Webshop.SL/Controllers/VatController.cs
+++ Webshop/Webshop.SL/Controllers/VatController.cs
@@ -4,10 +4,50 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Webshop.BL;
+using Webshop.Domain;
 
-namespace Webshop.SL.Controllers
+namespace Webshop.SL.Controller
 {
     public class VatController : ApiController
     {
+        private ILogic<VatDTO> _vatLogic;
+
+        public VatController(VatLogic logic)
+        {
+            _vatLogic = logic;
+        }
+
+        public IEnumerable<VatDTO> Get()
+        {
+            return _vatLogic.GetAll().AsEnumerable();
+        }
+
+        public VatDTO GetById(int id)
+        {
+            return _vatLogic.FindByID(id);
+        }
+
+        [HttpPost]
+        public void Create(VatDTO vatDto)
+        {
+            if (ModelState.IsValid)
+            {
+                _vatLogic.Create(vatDto);
+            }
+
+        }
+
+        [HttpPut]
+        public void Put(VatDTO vatDto)
+        {
+            _vatLogic.Update(vatDto);
+        }
+
+        [HttpDelete]
+        public void Delete(VatDTO vatDto)
+        {
+            _vatLogic.Delete(vatDto);
+        }
     }
 }
