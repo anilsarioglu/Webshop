2694f8f0aba3c196af42330f14610832dadb6d2a
Ismail <s104428@ap.be>
2019-11-27 14:34:38 +0100

    created pdf generator

--- Webshop/Webshop.UI-MVC/MapDTO.cs
+++ Webshop/Webshop.UI-MVC/MapDTO.cs
@@ -6,9 +6,9 @@ using System.Web;
 
 namespace Webshop.UI_MVC
 {
-    public static class MapDTO
+    internal static class MapDTO
     {
-        public static T1 Map<T1, T2>(T2 e)
+        internal static T1 Map<T1, T2>(T2 e)
         {
             var config = new MapperConfiguration(cfg => cfg.CreateMap<T2, T1>());
             var mapper = config.CreateMapper();
