c2c6ab4812ccd5799f8f4e4c1af8c819d765cdac
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-11-29 15:51:57 +0100

    small changes.

--- Webshop/Webshop.BL/EmailService.cs
+++ Webshop/Webshop.BL/EmailService.cs
@@ -9,16 +9,17 @@ namespace Webshop.BL
     {
         public void SendMail(string email, string id, string subject, string body)
         {
+            //TODO aanpassen waarden email!
             // Plug in your email service here to send an email.
             MailMessage mail = new MailMessage();
             SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            mail.From = new MailAddress("yaramis.naseyb@gmail.com");
+            mail.From = new MailAddress("xxxxxx@gmail.com");
             mail.To.Add(email);
             mail.Subject = subject;
             mail.Body = body;
 
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("yaramis.naseyb@gmail.com", "Nasy1995");
+            SmtpServer.Credentials = new System.Net.NetworkCredential("xxxx@gmail.com", "xxxx");
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
