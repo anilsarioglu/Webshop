5a957448b5f90366e48d18ccc23b4aa6b96f1a08
Ismail <s104428@ap.be>
2019-11-29 15:38:32 +0100

    added attachement option for email

--- Webshop/Webshop.BL/Services/EmailService.cs
+++ Webshop/Webshop.BL/Services/EmailService.cs
@@ -5,6 +5,22 @@ namespace Webshop.BL
 {
     public class EmailService
     {
+        public static void SendMail(String mailTo)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("teampuntkomma@gmail.com");
+            mail.To.Add(mailTo);
+            mail.Subject = "Test Mail";
+            mail.Body = "This is for testing SMTP mail from GMAIL";
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("teampuntkomma@gmail.com", "TeamPuntKomma3");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+        }
+
         public static void SendMail(String mailTo, String html)
         {
             MailMessage mail = new MailMessage();
