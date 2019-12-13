a860b1d26c1f9f206617ea52145d056459b9e28a
Ismail <s104428@ap.be>
2019-12-13 10:53:59 +0100

    Revert "remove unused code"
    This reverts commit 49c5ee0f7933c70cde774f2b48eaa498e3050259.

---
+++ Webshop/Webshop.DAL/Migrations/201911221419592_SeedProductPrice.cs
@@ -0,0 +1,16 @@
+﻿namespace Webshop.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedProductPrice : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
