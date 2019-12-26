72dfc8f846d90d3f5e8fcfe79cd92dbf1680ae6c
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-12-26 20:06:33 +0100

    Fixed migrations and small changes.

--- Webshop/Webshop.BL/Services/EmailService.cs
+++ Webshop/Webshop.BL/Services/EmailService.cs
@@ -13,13 +13,13 @@ namespace Webshop.BL
             // Plug in your email service here to send an email.
             MailMessage mail = new MailMessage();
             SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            mail.From = new MailAddress("xxxxx@gmail.com");
+            mail.From = new MailAddress("apschoolnet@gmail.com");
             mail.To.Add(email);
             mail.Subject = subject;
             mail.Body = body;
 
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("xxxxxx@gmail.com", "xxxxx");
+            SmtpServer.Credentials = new System.Net.NetworkCredential("apschoolnet@gmail.com", "apNet123");
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
