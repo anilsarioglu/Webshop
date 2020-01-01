73ceff50a2589e6f7ab84ac1728ef33ddd8d8b22
Anil Sarioglu <s109345@ap.be>
2020-01-01 14:31:58 +0100

    Created first (unfinished) version of Product Report

---
+++ Webshop/Webshop.UI-MVC/Controllers/ProductReportController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Webshop.UI_MVC.Models.Webshop;
+
+namespace Webshop.UI_MVC.Controllers
+{
+    public class ProductReportController : Controller
+    {
+        private const string PATH = "product";
+        IEnumerable<Product> products = APIConsumer<Product>.GetAPI(PATH);
+
+        // GET: ProductReport
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
